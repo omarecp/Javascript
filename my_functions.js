fb52b98c13bfe1940cbfeaa9c43f6f97cc084f38
Omar Castro <omarecp@hotmail.com>
2021-11-02 22:14:40 -0700

    Javascript functions learning

---
+++ my_functions.js
@@ -0,0 +1,22 @@
+function hello() {
+  console.log("Hello World!");
+}
+
+function addNum(num1,num2){
+  console.log(num1+num2);
+}
+
+function helloSomeone(name="guest")
+{
+  console.log("Hello" + name);
+}
+
+function formal(name ="Sam", title="Sir")
+{
+  return title+ "  "+name
+}
+
+function timesFive(numInput) {
+  var result = numInput *5
+  return result
+}
